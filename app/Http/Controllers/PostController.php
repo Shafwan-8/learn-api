6de3b46369844f111686a5a1439a5792aaf9458b
Wawanzz_ <arul.safwan73@gmail.com>
2024-02-28 16:30:49 +0700

    Done eps3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PostDetailResource;
+use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -9,8 +11,16 @@ class PostController extends Controller
 {
     public function index()
     {
-        $post = Post::all();
+        $posts = Post::all();
 
-        return response()->json($post);
+        // return response()->json(['data' => $posts]);
+
+        return PostResource::collection($posts);
+    }
+
+    public function show($id) {
+        $post = Post::with('writer:id,email,username')->findOrFail($id);
+
+        return new PostDetailResource($post);
     }
 }
---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'news_content' => $this->news_content,
+            'created_at' => date('d/m/Y H:i:s', strtotime($this->created_at,))
+        ];
+    }
+}
